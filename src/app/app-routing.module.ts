e624ef5bcf1fd42687a8e657e1eda4b2889824ad
Jean Jonathan <jeanjonathank2@gmail.com>
2023-04-09 21:21:55 +0200

    finalisation du projet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'interventions', component: InterventionListComponent },
   { path: 'new-intervention', component: InterventionFormComponent },
-  { path : '**',redirectTo:'home',pathMatch: 'full'}
+
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
