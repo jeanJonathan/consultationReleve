3ad505657b3749ed222658c82f3b672551149c76
Jean Jonathan <jeanjonathank2@gmail.com>
2023-04-09 22:17:58 +0200

    finalisation du projet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {ActivatedRoute, RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
 import {InterventionListComponent} from "./intervention-list/intervention-list.component";
 import {InterventionFormComponent} from "./intervention-form/intervention-form.component";
-import {HomeComponent} from "./home/home.component";
+import {CommonModule} from "@angular/common";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'interventions', component: InterventionListComponent },
   { path: 'new-intervention', component: InterventionFormComponent },
-
+  { path : '**', redirectTo:'home', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports:[
+  CommonModule,
+  RouterModule.forRoot([
+    { path: '', component: HomeComponent },
+    { path: 'interventions', component: InterventionListComponent },
+    { path: 'new-intervention', component: InterventionFormComponent }
+  ])],
+  exports: [RouterModule],
+  providers: [ActivatedRoute]
 })
 export class AppRoutingModule { }
